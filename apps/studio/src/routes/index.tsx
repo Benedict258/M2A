583c1f8403b08e2a9e4eb507eda5e7892d680ff8
Benedict258 <benedictisaac258@gmail.com>
2026-06-21 21:09:37 +0000

    Remove How It Works section (1. Build 2. Run 3. Dataset)

--- apps/studio/src/routes/index.tsx
+++ apps/studio/src/routes/index.tsx
@@ -4,7 +4,6 @@ import { LandingNavbar } from "@/components/landing-navbar";
 import HeroSection from "@/sections/HeroSection";
 import ProblemSection from "@/sections/ProblemSection";
 import SolutionSection from "@/sections/SolutionSection";
-import HowItWorksSection from "@/sections/HowItWorksSection";
 import FeaturesSection from "@/sections/FeaturesSection";
 import ArchitectureSection from "@/sections/ArchitectureSection";
 import DatasetsSection from "@/sections/DatasetsSection";
@@ -32,7 +31,6 @@ function LandingPage() {
       <HeroSection />
       <ProblemSection />
       <SolutionSection />
-      <HowItWorksSection />
       <FeaturesSection />
       <ArchitectureSection />
       <DatasetsSection />
